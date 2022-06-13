27ba4a4e51087355abe6871582d07c61a704f632
Yanivv77 <Yanivv77@gmail.com>
2022-06-13 16:24:16 +0300

    Tuple

--- 1-Tuple.cs
+++ 1-Tuple.cs
@@ -6,12 +6,27 @@ using System.Threading.Tasks;
 
 namespace C_Sharp
 {
+#pragma warning disable  // Variable is assigned but its value is never used
     internal class _1_Tuple
     {
 
         // Tuple - אוסף של משתנים שאפשר להעביר אותם ולהחזיר אותם מפונקציה בתור פרמטר אחד
 
-        public (int id, string fname, string lname ) CreatePerson()
+        public void Run()
+        {
+            int id;
+            string name;
+
+
+            (int id, string fname, string lname) var1;
+
+            var1 = (123, "Moshe", "Choen");
+
+            var newPerson = CreatePerson();
+
+
+        }
+        public (int id, string fname, string lname) CreatePerson()
         {
             int id = 12;
             string fname = "Moshe";
@@ -20,8 +35,8 @@ namespace C_Sharp
             var1.id = id;
             var1 = (id, fname, lname);
             return var1;
-
         }
+    }
 
     }
-}
+
