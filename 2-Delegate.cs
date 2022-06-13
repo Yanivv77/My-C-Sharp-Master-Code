90ffa040eb94683a12116dbdcb12169a1dfb8362
Yanivv77 <Yanivv77@gmail.com>
2022-06-13 16:02:47 +0300

    Delegate

---
+++ 2-Delegate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static C_Sharp._2_Delegate;
+
+namespace C_Sharp
+{
+
+    //Delegate  - הגדרה של חתימת פונקציה, ניתן לצור משתנים שיצביעו על פונקציה רלוונטית, רק לפי התאמת החתימה, ולהעביר כפרמטרים בכל פעם פונקציה שונה ובתנאי שתתאים לחתימה
+
+    public class _2_Delegate
+    {
+        public delegate int cluc(int n1, int n2);
+
+
+        public void run()
+        {
+         
+        }
+    }
+}
